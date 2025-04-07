5ca9c9961d74c841020715c2aba2432003e75a6a
dhakarRaghu <rdhakad2002@gmail.com>
2025-04-07 11:03:47 +0530

    update CORS origin and API base URL for production environment

--- frontend/src/helpers/api-communicators.ts
+++ frontend/src/helpers/api-communicators.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://creditsea-backend-2ibe.onrender.com/api",
   withCredentials: true, // Send cookies with requests
 });
 
