cb2b819a865fd8b5236cce0d3f92b66bd57a0497
dhakarRaghu <rdhakad2002@gmail.com>
2025-04-07 16:27:50 +0530

    feat: add a new endpoint for a simple hello world response

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,6 +22,10 @@ dotenv.config();
 
 app.use("/api", appRouter);
 
+app.get('/hello', (req, res) => {
+  res.send('Hello World!');
+}
+);
 app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
     console.error(err.stack);
     res.status(500).json({ message: 'Something went wrong!' });
